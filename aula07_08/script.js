ec2e96ddacf973223edcf23b3cfa819568f9ebce
NiloZeribinate <98361887+NiloZeribinate@users.noreply.github.com>
2022-08-27 19:46:21 -0300

    Update script.js

--- aula07_08/script.js
+++ aula07_08/script.js
@@ -2,8 +2,9 @@ function LoadPosts(){
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(function(resultado){
             return resultado.json
-        }).then(function(posts){
-            document.getElementById('posts').innerText = posts.length + ' posts'
+        })
+        .then(function(posts){
+            document.getElementById('posts').innerHTML = posts.length + ' posts'
         })
         .catch(function(erro){
             console.log('ERRO!')
