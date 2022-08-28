3e2954900ed19e6cb537bcf5968e83ced8d7f5fa
NiloZeribinate <98361887+NiloZeribinate@users.noreply.github.com>
2022-08-27 22:20:06 -0300

    Update script.js

--- aula07_08/script.js
+++ aula07_08/script.js
@@ -1,26 +1,14 @@
-/*function LoadPosts(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(function(resultado){
-            return resultado.json
-        })
-        .then(function(posts){
-            document.getElementById('posts').innerHTML = posts.length + ' posts'
-        })
-        .catch(function(error){
-            console.log('ERRO!')
-        })
-}*/
-
 function LoadPosts() {
     document.getElementById("posts").innerHTML = 'Carregando...';
     
     fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(function(resultado) {
-            return resultado.json();
-        }).then(function(json){
-            document.getElementById("posts").innerHTML = json.length+' posts';
+        .then(function(resultado){
+            return resultado.json()
         })
+            .then(function(posts){
+                document.getElementById('posts').innerHTML = posts.length + ' posts'
+            })
         .catch(function(error){
-            console.log("Deu problema!");
-        });
+            console.log('ERRO!')
+        })
 }
